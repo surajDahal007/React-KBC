8dcbc89b9ba0b56e43868986d45f144aedf7ecbd
surajDahal007 <engg.dahalsuraj@gmail.com>
2025-03-07 11:15:16 +0545

    Project Start

---
+++ src/Data/QuizData.js
@@ -0,0 +1,24 @@
+export const quizData = [
+  {
+    id: "1",
+    question: "What is the capital of Laos ?",
+    options: {
+      a: "Kathmandu",
+      b: "New Delhi",
+      c: "Seoul",
+      d: "Laos's capital",
+    },
+    answer: "d",
+  },
+  {
+    id: 2,
+    question: "What of the following language is most spoken globally? ",
+    options: {
+      a: "English",
+      b: "Mandarin(Chinese)",
+      c: "Spanish",
+      d: "French",
+    },
+    answer: 'b',
+  },
+];
